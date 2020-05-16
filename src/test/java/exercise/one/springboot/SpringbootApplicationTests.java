4df808339e8dc3aced1e7365dcf9846216e42c2c
thiago <thiagobalbino91@gmail.com>
2020-05-15 21:12:35 -0300

    teste iniciais

--- src/test/java/exercise/one/springboot/SpringbootApplicationTests.java
+++ src/test/java/exercise/one/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,28 @@
 package exercise.one.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import exercise.one.springboot.model.Pessoa;
+import exercise.one.springboot.repository.PessoaRepository;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+	
+	@Autowired
+	PessoaRepository pessoaRepository;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testeSalvar(){		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("maria");
+		pessoa.setSobrenome("jose");
+		pessoaRepository.save(pessoa);
 	}
 
 }
