8b57821cbbd31eb6dc35ab5f653b36b3bb1702b8
thiago <thiagobalbino91@gmail.com>
2020-05-16 09:02:20 -0300

    passagem parametro html para controller ok

--- src/main/java/exercise/one/springboot/controller/VeiculoController.java
+++ src/main/java/exercise/one/springboot/controller/VeiculoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import exercise.one.springboot.model.Veiculo;
 import exercise.one.springboot.service.VeiculoService;
@@ -24,6 +25,15 @@ public class VeiculoController {
 	public String salvar(Veiculo veiculo) {
 		veiculoService.salvar(veiculo);
 		return "cadastro/cadastroVeiculo"; //arquivo html
+	}
+	
+	@PostMapping(value = "/calcularCustoTotal")
+	public String calcularCustoTotal(
+						@RequestParam("distanciaRodoviaPavimentada") Double distanciaRodoviaPavimentada) {
+		
+		System.out.println("\n\n rp = " + distanciaRodoviaPavimentada);
+		return "index"; //arquivo html
+		
 	}
 	
 }
